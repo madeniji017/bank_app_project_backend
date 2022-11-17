199a5164c8ff4b39ac97b168aad64991ac59179b
khaleefahh <raheemibnsalam@gmail.com>
2022-11-17 15:14:25 +0100

    Removed multiple files

--- src/main/java/BnkApp/repository/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package BnkApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import BnkApp.entity.Account;
-
-public interface AccountRepo extends JpaRepository<Account, Long> {
-}
--- src/main/java/BnkApp/repository/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package BnkApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import BnkApp.entity.Customer;
-
-@Repository
-public interface CustomerRepo extends JpaRepository<Customer, Integer> {
-}
