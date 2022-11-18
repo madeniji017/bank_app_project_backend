c08aed276ca41565b23c399dc54692231277a65b
khaleefahh <raheemibnsalam@gmail.com>
2022-11-18 08:37:18 +0100

    Created ApiRequestException class and generated two default constructors

--- src/main/java/bank_app/exception/ApiRequestException.java
+++ src/main/java/bank_app/exception/ApiRequestException.java
@@ -1,2 +1,12 @@
-package bank_app.exception;public class ApiRequestException {
+package bank_app.exception;
+
+public class ApiRequestException extends RuntimeException {
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
