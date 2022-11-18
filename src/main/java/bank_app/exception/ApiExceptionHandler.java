e8caddcb07ec1a13094ce282b49f0591c563af98
khaleefahh <raheemibnsalam@gmail.com>
2022-11-18 21:33:40 +0100

    Removed the Throwable parameter from the ResponseEntity object

--- src/main/java/bank_app/exception/ApiExceptionHandler.java
+++ src/main/java/bank_app/exception/ApiExceptionHandler.java
@@ -19,7 +19,6 @@ public class ApiExceptionHandler {
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
         ApiException apiException = new ApiException(
                 e.getMessage(),
-                e,
                 badRequest,
                 ZonedDateTime.now(ZoneId.of("Z")));
 
