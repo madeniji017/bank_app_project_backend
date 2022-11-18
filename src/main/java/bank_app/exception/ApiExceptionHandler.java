6276fd6b967e822ef8f2beb4121166f6a45561a3
khaleefahh <raheemibnsalam@gmail.com>
2022-11-18 11:51:08 +0100

    Create the ResponseEntity method/object

--- src/main/java/bank_app/exception/ApiExceptionHandler.java
+++ src/main/java/bank_app/exception/ApiExceptionHandler.java
@@ -2,17 +2,20 @@ package bank_app.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+@RestControllerAdvice
 public class ApiExceptionHandler {
 
+    @ExceptionHandler(value = {ApiRequestException.class})
     public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
 
         //Create payload containing exception details
-
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
         ApiException apiException = new ApiException(
                 e.getMessage(),
