abb2de068747095210162ec18a8e8fc71c86a06d
khaleefahh <raheemibnsalam@gmail.com>
2022-11-22 12:43:45 +0100

    Implement the fetchUser  and createAccount interface methods

--- src/main/java/bank_app/service/BankServiceImpl.java
+++ src/main/java/bank_app/service/BankServiceImpl.java
@@ -1,10 +1,13 @@
 package bank_app.service;
 
+import bank_app.entity.Account;
 import bank_app.entity.User;
 import bank_app.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BankServiceImpl implements BankService{
@@ -22,6 +25,28 @@ public class BankServiceImpl implements BankService{
         user.setPassword(hashedPassword);
         
         return userRepo.save(user);
+    }
+
+    @Override
+    public User fetchUserById(Long id) {
+        return userRepo.findById(id).get();
+    }
+
+    @Override
+    public List<User> fetchUserList() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public Account createAccount(Long id) {
+        user = userRepo.findById(id).get();
+        Long userAcctNum = accountNumber.acctNumGenerator();
+        account = new Account(user, userAcctNum);
+        account.setAcctStatus("Active");
+        account.setUser(user);
+        accountRepo.save(account);
+
+        return account;
     }
 
 }
