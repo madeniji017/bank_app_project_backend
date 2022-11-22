7be6abd819706c1864f8e3cef42d8a208c7609ae
khaleefahh <raheemibnsalam@gmail.com>
2022-11-22 12:41:20 +0100

    Create the fetchUser  and createAccount interfaces

--- src/main/java/bank_app/service/BankService.java
+++ src/main/java/bank_app/service/BankService.java
@@ -1,8 +1,16 @@
 package bank_app.service;
 
+import bank_app.entity.Account;
 import bank_app.entity.User;
+
+import java.util.List;
 
 public interface BankService {
     User saveUser(User user);
 
+    User fetchUserById(Long id);
+
+    List<User> fetchUserList();
+
+    Account createAccount(Long id);
 }
