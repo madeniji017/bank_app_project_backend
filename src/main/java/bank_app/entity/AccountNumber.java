16958a746eafb7eb6e74cf0b89408e522dcbe9a0
khaleefahh <raheemibnsalam@gmail.com>
2022-11-24 10:32:17 +0100

    Use the synchronized keyword for the generateAcctNumber method

--- src/main/java/bank_app/entity/AccountNumber.java
+++ src/main/java/bank_app/entity/AccountNumber.java
@@ -1,24 +1,25 @@
 package bank_app.entity;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class AccountNumber {
 
     public long bankCode = 10;
     public long branchCode = 22;
-    public long customerCode = 100006;
+    public long customerCode = 100023;
 
-    @Bean
-    public Long acctNumGenerator() {
+
+    public synchronized Long generateAcctNumber() {
         String tempAcctNum = String.format("%s%s%s", bankCode, branchCode, customerCode);
-        System.out.println(tempAcctNum);
         Long l = Long.parseLong(tempAcctNum);
         customerCode++;
         return l;
     }
 
     public AccountNumber() {}
+
+
 
 }
