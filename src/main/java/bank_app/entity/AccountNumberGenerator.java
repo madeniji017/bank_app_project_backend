5258ce5a65ab7bbfc78ac94170ba4f6129129ec3
khaleefahh <raheemibnsalam@gmail.com>
2022-11-24 15:38:58 +0100

    Change instance field access types. +More annotations

--- src/main/java/bank_app/entity/AccountNumberGenerator.java
+++ src/main/java/bank_app/entity/AccountNumberGenerator.java
@@ -1,17 +1,21 @@
 package bank_app.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-
+@Getter
+@Setter
 @Entity
 public class AccountNumberGenerator {
 
     @Id
     private int id;
-    public long bankCode;
-    public long branchCode;
-    public long customerCode;
+    private long bankCode;
+    private long branchCode;
+    private long customerCode;
 
 
     public synchronized Long generateAcctNumber() {
