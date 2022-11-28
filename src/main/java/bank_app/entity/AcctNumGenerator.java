09d561a0268fd9ca4ac2480e6412f54393cd9896
khaleefahh <raheemibnsalam@gmail.com>
2022-11-28 10:44:09 +0100

    Create the class to generate account number from DB

---
+++ src/main/java/bank_app/entity/AcctNumGenerator.java
@@ -0,0 +1,26 @@
+package bank_app.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Component
+@Setter
+@Getter
+@Entity
+public class AcctNumGenerator {
+
+    @Id
+    private Long id;
+    private Long bankCode;
+    private Long branchCode;
+    private Long customerCode;
+
+
+    public AcctNumGenerator() {}
+
+
+}
---
+++ src/main/java/bank_app/repo/AcctNumGeneratorRepo.java
@@ -0,0 +1,10 @@
+package bank_app.repo;
+
+import bank_app.entity.AcctNumGenerator;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AcctNumGeneratorRepo extends GenericRepo<AcctNumGenerator> {
+}
+
+
