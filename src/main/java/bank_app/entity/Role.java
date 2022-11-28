0d9767ec0ba72bcac77b5592f602347c10fffd36
khaleefahh <raheemibnsalam@gmail.com>
2022-11-28 10:33:42 +0100

    Change Id generation type to IDENTITY

--- src/main/java/bank_app/entity/Role.java
+++ src/main/java/bank_app/entity/Role.java
@@ -15,13 +15,12 @@ import java.util.List;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String type;
 
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "role")
-    //@JsonManagedReference
     private List<User> user;
 
 }
