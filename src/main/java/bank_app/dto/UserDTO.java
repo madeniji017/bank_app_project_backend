6415aeb73912cde0008e3878b2dec40a14227f72
khaleefahh <raheemibnsalam@gmail.com>
2022-11-28 19:15:47 +0100

    Create the userDTO class

---
+++ src/main/java/bank_app/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package bank_app.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String address;
+
+    //private char gender;
+
+    private String username;
+
+    private String password;
+
+}
