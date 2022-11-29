bb114baa76c8b53fd21b7f7902dce70e277f27e4
khaleefahh <raheemibnsalam@gmail.com>
2022-11-29 10:30:05 +0100

    Refactor methods to use DTOs

--- src/main/java/bank_app/service/BankService.java
+++ src/main/java/bank_app/service/BankService.java
@@ -1,20 +1,21 @@
 package bank_app.service;
 
+import bank_app.dto.UserDTO;
 import bank_app.entity.Account;
 import bank_app.entity.User;
 
 import java.util.List;
 
 public interface BankService {
-    User saveUser(User user);
+    UserDTO saveUser(UserDTO userDTO);
 
     User fetchUserById(Long id);
 
-    List<User> fetchUserList();
+    List<UserDTO> fetchUserList();
 
     Account createAccount(Long id);
 
     void deleteAccountById(Long id);
 
-    User updateUser(Long id, User userUpdate);
+    UserDTO updateUser(Long id, UserDTO userUpdate);
 }
