00600f8e0e0ab25830c96d86b1c37d6253f204b6
khaleefahh <raheemibnsalam@gmail.com>
2022-12-14 07:55:51 +0100

    Comment out the print statement

--- src/main/java/bank_app/SpringbootBankAppApplication.java
+++ src/main/java/bank_app/SpringbootBankAppApplication.java
@@ -3,6 +3,8 @@ package bank_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringbootBankAppApplication {
@@ -10,6 +12,21 @@ public class SpringbootBankAppApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringbootBankAppApplication.class, args);
+
+		/*
+		InetAddress ip;
+		String hostname;
+		try {
+			ip = InetAddress.getLocalHost();
+			hostname = ip.getHostName();
+			System.out.println("Your current IP address : " + ip);
+			System.out.println("Your current Hostname : " + hostname);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		 */
+
 	}
 
 }
