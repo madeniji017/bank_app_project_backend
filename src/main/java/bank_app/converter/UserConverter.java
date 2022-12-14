99a48075c490cec5ee953f60e5f3db103126b7dd
khaleefahh <raheemibnsalam@gmail.com>
2022-12-14 07:41:52 +0100

    Modify the convertEntityDto method

--- src/main/java/bank_app/converter/UserConverter.java
+++ src/main/java/bank_app/converter/UserConverter.java
@@ -3,8 +3,10 @@ package bank_app.converter;
 import bank_app.dto.UserDTO;
 import bank_app.entity.User;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,15 +15,18 @@ public class UserConverter {
 
     public UserDTO convertEntityToDto(User user) {
 
-        ModelMapper modelMapper = new ModelMapper();
-
-        return modelMapper.map(user, UserDTO.class);
+//        ModelMapper modelMapper = new ModelMapper();
+//        return modelMapper.map(user, UserDTO.class);
+        UserDTO dto = new UserDTO();
+        BeanUtils.copyProperties(user, dto);
+        return dto;
 
     }
 
     public List<UserDTO> convertEntityListToDto(List<User> userList) {
 
         //lambda is replaced here with method reference
+        List<User> user =  new ArrayList<>();
         return userList.stream().map(this::convertEntityToDto).collect(Collectors.toList());
     }
 
