883d166f5b9a952963099f12b3814d08e5208879
khaleefahh <raheemibnsalam@gmail.com>
2022-12-15 12:17:08 +0100

    Create exception handler class and method

--- src/main/java/bank_app/error/RestResponseEntityExceptionHandler.java
+++ src/main/java/bank_app/error/RestResponseEntityExceptionHandler.java
@@ -1,2 +1,26 @@
-package bank_app.error;public class RestResponseEntityExceptionHandler {
+package bank_app.error;
+
+import bank_app.entity.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+@ResponseStatus
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorMessage> userNotFoundException(UserNotFoundException exception,
+                                                             WebRequest webRequest) {
+        ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+    }
+
+
 }
--- src/main/java/bank_app/error/UserNotFoundException.java
+++ src/main/java/bank_app/error/UserNotFoundException.java
@@ -1,2 +1,23 @@
-package bank_app.error;public class UserNotFoundException {
+package bank_app.error;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
